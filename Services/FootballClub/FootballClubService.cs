df90c010d88dc7b0da671cf52378d3483dce070d
marko vujic <marko.vujic@comparis.ch>
2022-06-15 16:43:23 +0200

    update FootbalClub service with method for add and get entities from db

--- Services/FootballClub/FootballClubService.cs
+++ Services/FootballClub/FootballClubService.cs
@@ -3,51 +3,35 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using AutoMapper;
+using dotnet_rpg.Data;
 using dotnet_rpg.Dtos.FootballClub;
 using dotnet_rpg.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace dotnet_rpg.Services.FootballClub
 {
     public class FootballClubService : IFootballClubService
     {
         private IMapper _mapper;
-        private IList<Models.FootballClub> _clubs = new List<Models.FootballClub>
-        {
-            new Models.FootballClub
-            {
-                Competitions = null,
-                Country = "Serbia",
-                Id = 1,
-                Stadium = "Rajko Mitic",
-                Name = "Crvena Zvezda"
-            },
-            new Models.FootballClub
-            {
-                Competitions = null,
-                Country = "Serbia",
-                Id = 2,
-                Stadium = "JNA",
-                Name = "Partizan"
-            }
-        };
+        private readonly FootballWorldDataContext _footballWorldDataContext;
 
-        public FootballClubService(IMapper mapper)
+        public FootballClubService(IMapper mapper, FootballWorldDataContext footballWorldDataContext)
         {
             _mapper = mapper;
+            _footballWorldDataContext = footballWorldDataContext;
         }
 
-        public Task AddFootballClub(AddFootballClubDto footballClubDto)
+        public async Task AddFootballClub(AddFootballClubDto footballClubDto)
         {
             var newClub = _mapper.Map<Models.FootballClub>(footballClubDto);
-            newClub.Id = _clubs.Max(x => x.Id) + 1;
-            _clubs.Add(newClub);
-            return Task.CompletedTask;
+            _footballWorldDataContext.FootballClubs.Add(newClub);
+            await _footballWorldDataContext.SaveChangesAsync();
         }
 
         public async Task<ServiceResponse<GetFootballClubDto>> UpdateFootballClub(UpdatedFootballClubDto updatedFootballClub)
         {
             var serviceResponse = new ServiceResponse<GetFootballClubDto>();
-            var club = _clubs.FirstOrDefault(x => x.Id == updatedFootballClub.Id);
+            var club = _footballWorldDataContext.FootballClubs.FirstOrDefault(x => x.Id == updatedFootballClub.Id);
 
             if (club != null)
             {
@@ -68,14 +52,14 @@ namespace dotnet_rpg.Services.FootballClub
 
         public async Task<ServiceResponse<List<GetFootballClubDto>>> DeleteFootballClubById(int id)
         {
-            var footballClub = _clubs.FirstOrDefault(x => x.Id == id);
+            var footballClub = _footballWorldDataContext.FootballClubs.FirstOrDefault(x => x.Id == id);
             var serviceResponse = new ServiceResponse<List<GetFootballClubDto>>();
 
             if (footballClub != null)
             {
-                _clubs.Remove(footballClub);
+                _footballWorldDataContext.FootballClubs.Remove(footballClub);
 
-                serviceResponse.Data = _mapper.Map<List<GetFootballClubDto>>(_clubs.ToList());
+                serviceResponse.Data = _mapper.Map<List<GetFootballClubDto>>(_footballWorldDataContext.FootballClubs.ToList());
 
                 return serviceResponse;
             }
@@ -89,14 +73,25 @@ namespace dotnet_rpg.Services.FootballClub
         public async Task<ServiceResponse<List<GetFootballClubDto>>> GetAllFootballClubs()
         {
             var serviceResponse = new ServiceResponse<List<GetFootballClubDto>>();
-            serviceResponse.Data = _mapper.Map<List<GetFootballClubDto>>(_clubs.ToList());
+            var footballClubs = await _footballWorldDataContext.FootballClubs.AsQueryable().ToListAsync();
+            serviceResponse.Data = _mapper.Map<List<GetFootballClubDto>>(footballClubs);
             return serviceResponse;
         }
 
         public async Task<ServiceResponse<GetFootballClubDto>> GetFootballClubById(int id)
         {
             var serviceResponse = new ServiceResponse<GetFootballClubDto>();
-            serviceResponse.Data = _mapper.Map<GetFootballClubDto>(_clubs.FirstOrDefault(x => x.Id == id));
+            var footballClub = await _footballWorldDataContext.FootballClubs.AsQueryable()
+                .FirstOrDefaultAsync(x => x.Id == id);
+            if (footballClub != null)
+            {
+                serviceResponse.Data = _mapper.Map<GetFootballClubDto>(footballClub);
+
+                return serviceResponse;
+            }
+
+            serviceResponse.Success = false;
+            serviceResponse.Message = $"Entity with {id} is not found";
             return serviceResponse;
         }
     }
